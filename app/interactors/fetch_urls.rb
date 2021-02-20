c540b79187d07be95ba1b09e43a8bff9e274046a
Alexander <cyberxander90@gmail.com>
2021-02-20 00:42:22 -0300

    Fetch urls returns the last 10 urls

--- app/interactors/fetch_urls.rb
+++ app/interactors/fetch_urls.rb
@@ -2,6 +2,6 @@ class FetchUrls
   include Interactor
 
   def call
-    context.urls = Url.all
+    context.urls = Url.order(created_at: :asc).last(10)
   end
 end
--- spec/interactors/fetch_urls_spec.rb
+++ spec/interactors/fetch_urls_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe FetchUrls, type: :interactor do
   subject { FetchUrls.call }
 
   describe '.call' do
-    let(:urls) { double() }
-    before { allow(Url).to receive(:all).and_return(urls) }
+    let!(:now) { Time.now }
+    let!(:urls) { (1..15).map { |i| create(:url, created_at: now + i.minutes) } }
 
     it_behaves_like :context_success
 
-    it 'return the URLs' do
-      expect(subject.urls).to eq(urls)
+    it 'return the last 10 URLs' do
+      expect(subject.urls).to eq(urls[5..])
     end
   end
 end
