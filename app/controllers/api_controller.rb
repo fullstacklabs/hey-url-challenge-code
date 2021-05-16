3c3e68951a7156abdad147e94dfeae84dd0967b7
Fausto Santin <fausto.santin@neoway.com.br>
2021-05-16 04:55:10 -0300

    challenge: application controller tests and functionallity implementated.

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::API
+
+  def last
+
+    render json: []
+  end
+end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+require 'uri'
+
+class UrlValidator < ActiveModel::Validator
+  def validate(record)
+    record.errors.add :empty, "Short url is empty" if record.short_url.nil?
+    record.errors.add :regex, "Short url does not match 5 capital letters" if not record.short_url.match? /^[A-Z]{5}$/
+    record.errors.add :empty, "Original url is empty" if record.original_url.nil?
+    record.errors.add :empty, "Original url is not valid" if not record.original_url.match? /\A#{URI::regexp(['http', 'https'])}\z/
+  end
+end
+
 class Url < ApplicationRecord
   # scope :latest, -> {}
+  has_many :clicks
+  validates_with UrlValidator
 end
